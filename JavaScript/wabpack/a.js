a8c0d105e8dc6ad077671d6304f880a8791ec5c6
shaosheng <yolanda@4.me>
2019-04-28 15:21:44 +0800

    some

---
+++ JavaScript/wabpack/a.js
@@ -0,0 +1,5 @@
+import {foo} from './b.js'
+import {bar} from './b.js'
+
+console.log(foo())
+console.log(bar())
---
+++ JavaScript/wabpack/b.js
@@ -0,0 +1,7 @@
+export function foo() {
+  return 1
+}
+
+export function bar(a,b) {
+  return a * b + 'foobar'
+}
